c6462729c981c68d45ea0d237201b82cc712c561
bettyit <88734420+bettyit@users.noreply.github.com>
2021-08-10 16:14:28 -0500

    Create Hola.java

---
+++ Hola.java
@@ -0,0 +1,7 @@
+public class Hola {
+    public static void main(String[] args) {
+        System.out.println("Running HelloWorld into docker");
+        System.out.println("Hello World Docker!!!");
+        System.out.println("Bye bye!");
+    }
+}
