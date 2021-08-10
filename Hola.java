6f28ae8999e9abe0cfb2144356070089d51ea920
bettyit <88734420+bettyit@users.noreply.github.com>
2021-08-10 16:20:29 -0500

    Delete Hola.java

--- Hola.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Running HelloWorld into docker");
-        System.out.println("Hello World Docker!!!");
-        System.out.println("Bye bye!");
-    }
-}
